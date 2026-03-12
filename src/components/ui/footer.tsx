5479ec4c40076fdaa35d6b189323a9893b97aaf7
Chapiuskys <gabrielmp2709@gmail.com>
2026-03-12 20:25:41 -0300

    feat: initial commit - e-commerce setup

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+      <div className="mx-auto lg:container">
+        © 2026 Copyright{" "}
+        <span className="font-semibold">PurpleTech Store</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

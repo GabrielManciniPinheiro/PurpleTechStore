03b21c46b0599481f9c1cadc08043557a028d2d0
Chapiuskys <gabrielmp2709@gmail.com>
2026-03-20 23:56:57 -0300

    feat: arrumando a adicao de numero de pedido no arquivo order.ts

--- src/actions/order.ts
+++ src/actions/order.ts
@@ -7,10 +7,14 @@ export const createOrder = async (
   cartProducts: CartProduct[],
   userId: string,
 ) => {
+  // 👇 1. Criamos a variável que gera o código aleatório (Ex: PT-4829)
+  const generatedOrderNumber = `PT-${Math.floor(1000 + Math.random() * 9000)}`;
+
   const order = await prismaClient.order.create({
     data: {
       userId,
       status: "WAITING_FOR_PAYMENT",
+      orderNumber: generatedOrderNumber, // 👇 2. Injetamos o número obrigatório aqui!
       orderProducts: {
         createMany: {
           data: cartProducts.map((product) => ({
--- src/app/(shop)/orders/helpers/status.ts
+++ src/app/(shop)/orders/helpers/status.ts
@@ -2,7 +2,10 @@ import { OrderStatus } from "@prisma/client";
 
 export const getOrderStatus = (orderStatus: OrderStatus) => {
   return {
-    [OrderStatus.PAYMENT_CONFIRMED]: "Pago",
     [OrderStatus.WAITING_FOR_PAYMENT]: "Pendente",
+    [OrderStatus.PAYMENT_CONFIRMED]: "Pago",
+    [OrderStatus.SHIPPED]: "Enviado",
+    [OrderStatus.DELIVERED]: "Entregue",
+    [OrderStatus.CANCELED]: "Cancelado",
   }[orderStatus];
 };
